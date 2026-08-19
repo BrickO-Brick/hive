4cd8544f2b5be3168ea2a13f6ae28f5dba5d7d61
loganj <loganj@squareup.com>
2026-08-19 17:08:18 +0000

    test(desktop): seed avatar for active mock identity
    Signed-off-by: loganj <loganj@squareup.com>

--- desktop/tests/e2e/messaging.spec.ts
+++ desktop/tests/e2e/messaging.spec.ts
@@ -378,7 +378,7 @@ test.beforeEach(async ({ page }, testInfo) => {
           ? {
               searchProfiles: [
                 {
-                  pubkey: TEST_IDENTITIES.tyler.pubkey,
+                  pubkey: "deadbeef".repeat(8),
                   displayName: "tyler",
                   avatarUrl:
                     'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"%3E%3Crect width="16" height="16" rx="4" fill="%2300a36c"/%3E%3C/svg%3E',
